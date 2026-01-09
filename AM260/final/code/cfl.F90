e658a871108752b5fcd12e828a83b948ae82a48d
dbuhl <dbuhl@ucsc.edu>
2026-01-09 10:24:35 -0800

    fixed cfl, need to work on output

--- AM260/final/code/cfl.F90
+++ AM260/final/code/cfl.F90
@@ -8,20 +8,22 @@ subroutine cfl(dt)
   implicit none
   real, intent(OUT) :: dt
   integer :: i
-  real :: maxSpeed, lambda, cs
+  real :: maxSpeed, lambda, cs, maxDt
 
   maxSpeed = 1e-8
+  maxDt = 0.01
   !! update conservative vars
   do i = gr_ibeg, gr_iend
     ! this needs to be changes to include all wave speeds (pressure, dens,
     ! and velx)
      cs = sqrt(gr_V(GAMC_VAR,i)*gr_V(PRES_VAR,i)/gr_V(DENS_VAR,i))
-     lambda=(abs(gr_V(VELX_VAR,i)) + cs)
+     lambda=(maxval(abs(gr_V(1:3,i))) + cs)
      maxSpeed=max(maxSpeed,lambda)
   end do
 
   ! cfl
   dt = sim_cfl*gr_dx/maxSpeed
+  dt = min(dt,maxDt)
 
   return
 
